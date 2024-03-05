87bf3e10910c6f81de13009905631253a268af06
Alice Stears <alice.e.stears@gmail.com>
2024-03-05 16:56:02 -0700

    setting up R project and file structure

---
+++ Analysis/DataExploration.R
@@ -0,0 +1,10 @@
+#///////////////////
+# Importing and exploring potential data sources
+# Alice Stears
+# 04/05/2024
+#///////////////////
+
+
+# load packages -----------------------------------------------------------
+
+library(tidyverse)
