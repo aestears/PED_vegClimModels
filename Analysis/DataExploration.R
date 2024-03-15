0b0ca9621472dc2af9c3f4438a6b493cb14d6ddb
Alice Stears <alice.e.stears@gmail.com>
2024-03-15 15:11:48 -0600

    downloading AIM data

--- Analysis/DataExploration.R
+++ Analysis/DataExploration.R
@@ -6,13 +6,15 @@
 
 
 # load packages -----------------------------------------------------------
-
+#devtools::install_github("landscape-data-commons/trex", build_vignettes = TRUE)
+library(trex)
 library(tidyverse)
 library(geojsonsf)
 
 
 # AIM data exploration ----------------------------------------------------
-
+### this is raw AIM data, I think? not actually quite sure... will move to 
+# getting data form the Landscape Data Commons, per Gregor's advice
 ### read in LMF data 
 AIM_LMF <- geojson_sf("./data/AIM_BLM_Natl_AIM_LMF_Hub/BLM_Natl_AIM_LMF_Hub_-6152108000941427991.geojson") %>% 
   mutate(Date_visited_new = str_sub(DateVisited,  start = 6, end = 16))
@@ -31,4 +33,8 @@ ggplot() +
   geom_sf(data = AIM_LMF, aes(color = as.factor(lubridate::year(Date_visited_new))), pch = 18, alpha = .5) + 
   geom_sf(data = AIM_TDat, aes(color = as.factor(lubridate::year(Date_visited_new))), pch = 1, alpha = .5) +
   guides(fill = guide_none())
+
+
+# Get AIM Data using trex R package ---------------------------------------
+trex::fetch_ldc()
 
