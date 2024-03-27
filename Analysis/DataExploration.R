13a0b640aff7faa93b71ac715f8dfac745fcf1e3
Alice Stears <alice.e.stears@gmail.com>
2024-03-27 14:00:11 -0600

    added species names to grass data to then use to add photosynthetic pathway data

--- Analysis/DataExploration.R
+++ Analysis/DataExploration.R
@@ -36,6 +36,10 @@ ggplot() +
 
 
 # Get AIM Data using trex R package ---------------------------------------
+# acronyms in LDC data: "FH" = "%First Hit" (the percentage of the first hit that had that species??)
+# "HGT" = height 
+# "AH" = "all hits"? maybe? 
+
 # get "indicators" data, for all plots!  (keys argument)
 # indicatorDat <- trex::fetch_ldc(#keys = "17073113365985482017-09-01", key_type = "PrimaryKey",
 #                 data_type = "indicators") %>% 
@@ -65,3 +69,111 @@ unique(lubridate::year(indicatorDat$DateVisited))
 # visualize the location of plots
 ggplot(indicatorDat[1:20000,]) + 
   geom_point(aes(x = Longitude_NAD83, y = Latitude_NAD83, color = TotalFoliarCover))
+
+# what do the distributions of percent cover data look like? 
+# total foliar cove
+hist(indicatorDat$TotalFoliarCover) # pretty normally distributed between 0 and 100
+
+# I think other functional group cover values are all very right skewed
+hist(indicatorDat$AH_AnnGrassCover) # very right skewed
+hist(indicatorDat$AH_PerenGrassCover) # very right skewed
+hist(indicatorDat$AH_PerenForbCover)
+
+# do cover values ever sum above 100%?
+summedCover <- indicatorDat %>% 
+  select(AH_ForbCover, AH_GrassCover, AH_ShrubCover) %>% 
+  rowSums()
+hist(summedCover)
+
+  
+
+# Calculate % Cover by c3 vs c4 grass -------------------------------------
+# can just aggregate from Species-level data (double check that this is accurate, with shrubs, for example!)
+shrubTest <- speciesDat %>% 
+  filter(GrowthHabitSub == "Shrub") %>% 
+  group_by(PrimaryKey) %>% 
+  summarize("AH_shrub_sum" = sum(AH_SpeciesCover, na.rm = TRUE))
+# compare to actual data in indicatorDat
+testTest <- indicatorDat %>% 
+  select(PrimaryKey, AH_ShrubCover) %>% 
+  left_join(shrubTest)
+
+# The values are the same! Can proceed w/ DIY grouping of grasses by C3 vs. 
+# C4 using by aggregating the speciesDat data frame
+# get list of grass species for which we need C3/C4 data
+grassSpp <- speciesDat %>% 
+  filter(GrowthHabitSub == "Graminoid" & !is.na(AH_SpeciesCover)) %>% 
+  select(Species) %>% 
+  unique()
+
+## problem... there are quite a few "species" that are just called "perennial 
+# grass" which I obviously can't assign a photosynthetic pathway to. Should I 
+# just remove plots that have these "species"? There are quite a few of these 
+# observations that have a large % cover, so I definitely don't want to just throw them out
+# find the 'bad quadrats' that have these weird species names so that we can remove them 
+PG_data <- speciesDat %>% 
+  filter(str_detect(speciesDat$Species, pattern = "^PG"))
+# get plot names that have >2% of weird species names 
+PG_data <- PG_data %>% 
+  filter(AH_SpeciesCover > 2)
+badQuads <- unique(PG_data$PrimaryKey)                   
+
+# remove those plots from the data frame of grass species
+grassDat <- speciesDat %>% 
+  filter(GrowthHabitSub == "Graminoid" &         
+           !is.na(AH_SpeciesCover) &
+    !(PrimaryKey %in% badQuads))
+
+# remove observations for weird species names w/ % cover less than 2%
+grassDat <- grassDat %>% 
+  filter(!str_detect(Species, pattern = "^PG"))
+
+##
+# also issues with "names" starting with "AG" (which I think means Annual Grass)
+AG_data <- speciesDat %>% 
+  filter(str_detect(speciesDat$Species, pattern = "^AG"))
+# get plot names that have >2% of weird species names 
+AG_data <- AG_data %>% 
+  filter(AH_SpeciesCover > 2)
+badQuads <- unique(AG_data$PrimaryKey)                   
+
+# remove those plots from the data frame of grass species
+grassDat <- grassDat %>% 
+  filter(GrowthHabitSub == "Graminoid" &    
+           !(PrimaryKey %in% badQuads))
+
+# remove observations for weird species names w/ % cover less than 2%
+grassDat <- grassDat %>% 
+  filter(!str_detect(Species, pattern = "^AG"))
+
+## now get the unique names in the dataset
+grassNames <- unique(grassDat$Species) %>% 
+  data.frame() 
+names(grassNames) <- "Species"
+# get the species names lookup table from BLM 
+taxCodes <- read.csv("./data/USDA_plants_SppNameTable.csv")
+# deal w/ synonym issues (try to, anyway)
+uniqueSymbols <- unique(taxCodes$AcceptedSymbol)
+for (i in 1:length(uniqueSymbols)) {
+  temp <- taxCodes[taxCodes$AcceptedSymbol == uniqueSymbols[i],]
+  temp$Status <- NA
+  temp[temp$SynonymSymbol != "","Status"] <- "Synonym"
+  temp[temp$SynonymSymbol == "","Status"] <- "Accepted"
+  if (i == 1) {
+    taxCodes_new <- temp
+  } else {
+    taxCodes_new <- rbind(taxCodes_new, temp)
+  }
+}
+
+# save the synonyms data
+#write.csv(taxCodes_new, "./data/USDA_plants_SppNameTable_synonymNames.csv", row.names = TRUE)
+
+# write.csv(taxCodes, file = "./data/USDA_plants_SppNameTable_New.csv", row.names = FALSE)
+# use to get species latin names 
+grassDat <- left_join(grassDat, taxCodes_new[taxCodes_new$Status == "Accepted",], 
+                      by = c("Species"= "AcceptedSymbol")) %>% 
+  select(-SynonymSymbol)
+
+
+
