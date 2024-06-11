234675aead9f7afa3f3fa8fe5bc6289b290a72cb
Alice Stears <alice.e.stears@gmail.com>
2024-06-11 11:58:25 -0600

    add datasets together and visualize spatial distributions

--- Analysis/MakingBigCompositionDataset.R
+++ Analysis/MakingBigCompositionDataset.R
@@ -32,9 +32,9 @@ FIA_all <- FIA_veg %>%
          NonTallyTree_AerialCover, PCTBARE_RMRS, SLOPE, ASPECT, STATENAME, 
          LAT, LON, Source, Litter_PctCover, BareGround_PctCover, MEASYEAR, 
          DuffDepth, LitterDepth, FuelDepth, basalArea_allGroups_in2,
-         basalArea_Angiosperms_perc, basalArea_Gymnosperms_perc) %>%  #remove columns we don't need
+         basalArea_Angiosperms_perc, basalArea_Gymnosperms_perc, LitterDepth) %>%  #remove columns we don't need
   transmute(UniqueID = PLT_CN, 
-         StateUnitCountyPlot = paste0(STATECD, "_", UNITCD, "_", COUNTYCD),
+         StateUnitCounty = paste0(STATECD, "_", UNITCD, "_", COUNTYCD),
          Plot = PLOT, 
          PlotCondition = CONDID,
          date = INVYR, 
@@ -49,11 +49,23 @@ FIA_all <- FIA_veg %>%
          ConifTreeCover = TallyTree_AerialCover * basalArea_Gymnosperms_perc,
          TotalTreeCover = TallyTree_AerialCover + NonTallyTree_AerialCover,
          TreeBasalArea_in2 = basalArea_allGroups_in2, 
-         BareGroundCover = NA, 
-         LitterCover = NA
+         BareGroundCover_temp = BareGround_PctCover, 
+         PCTBARE_RMRS = PCTBARE_RMRS, 
+         LitterCover = Litter_PctCover,
+         LitterDepth = LitterDepth,
+         Source = "FIA"
          ) # update/change some names
 
-#AES need to add in litter and ground cover data 
+# average columns for BareGroundCover and PCTBARE_RMRS
+FIA_all$BareGroundCover  <- apply(FIA_all[,c("BareGroundCover_temp", "PCTBARE_RMRS")], MARGIN = 1, FUN = function(x) 
+  mean(x, na.rm = TRUE))
+FIA_all <- FIA_all %>% 
+  select(-BareGroundCover_temp, -PCTBARE_RMRS) %>% 
+  mutate(BareGroundCover = replace(BareGroundCover, BareGroundCover == 999, NA)) %>% 
+  select(UniqueID, StateUnitCounty, Plot,  PlotCondition, date,  Lat,  Lon,
+         ShrubCover, HerbCover,  TotalGramCover, C3GramCover,  C4GramCover, 
+         AngioTreeCover ,   ConifTreeCover, TotalTreeCover, TreeBasalArea_in2, 
+         BareGroundCover,   LitterCover,  LitterDepth, Source)
 # load LANDFIRE data ------------------------------------------------------
 
 LANDFIRE_veg <- read.csv("./data/LANDFIRE_LFRDB/coverDat_USE.csv") %>% 
@@ -69,7 +81,7 @@ LANDFIRE_all <- LANDFIRE_veg %>%
          PlotCondition = NA, 
          date = date,
          Lat = Lat,
-         Long = Long,
+         Lon = Long,
          ShrubCover = LFShrubCov, 
          HerbCover = LFHerbCov, 
          TotalGramCover = C3_LFAbsCov + C4_LFAbsCov,
@@ -80,7 +92,9 @@ LANDFIRE_all <- LANDFIRE_veg %>%
          TotalTreeCover = AngioTree_LFAbsCov + ConifTree_LFAbsCov,
          TreeBasalArea_in2 = NA, 
          BareGroundCover = NA, 
-         LitterCover = NA
+         LitterCover = NA,
+         LitterDepth = NA,
+         Source = "LANDFIRE"
          ) 
 
 # load Landscape Data Commons data ----------------------------------------
@@ -98,7 +112,7 @@ LDC_all <- LDC_veg %>%
             PlotCondition = NA, 
             date = date,
             Lat = Latitude_NAD83,
-            Long = Longitude_NAD83,
+            Lon = Longitude_NAD83,
             ShrubCover = AH_ShrubCover, 
             HerbCover = AH_ForbCover, 
             TotalGramCover = AH_C3TotalCover + AH_C4TotalCover,
@@ -109,9 +123,44 @@ LDC_all <- LDC_veg %>%
             TotalTreeCover = AH_AngioTotalCover + AH_ConifTotalCover,
             TreeBasalArea_in2 = NA, 
             BareGroundCover = BareSoilCover, 
-            LitterCover = FH_TotalLitterCover
+            LitterCover = FH_TotalLitterCover,
+            LitterDepth = NA,
+            Source = "LDC"
               ) 
 
 # add datasets together ---------------------------------------------------
+dat_all <- FIA_all %>% 
+  rbind(LANDFIRE_all, LDC_all)
 
+# plot of all data points
+plot(dat_all$Lon, dat_all$Lat, col = as.factor(dat_all$Source))
+ggplot(dat_all) +
+  geom_point(aes(Lon, Lat, col = Source), alpha = .5)
 
+# plot of data that has c3/c4 and broad leaf vs. conifer
+dat_all %>% 
+  filter(!is.na(ConifTreeCover) & 
+           !is.na(AngioTreeCover) &
+           !is.na(C3GramCover) & 
+           !is.na(C4GramCover)) %>% 
+  ggplot() +
+  geom_point(aes(Lon, Lat, col = Source), alpha = .5) + 
+  scale_color_manual(values = c("forestgreen", "cornflowerblue"))
+
+# plot of data that have litter cover
+dat_all %>% 
+filter(!is.na(LitterCover)) %>% 
+  ggplot() +
+  geom_point(aes(Lon, Lat, col = Source), alpha = .5)
+
+# plot of data that have litter depth
+dat_all %>% 
+  filter(!is.na(LitterDepth)) %>% 
+  ggplot() +
+  geom_point(aes(Lon, Lat, col = Source), alpha = .5) 
+
+# plot of data that have bare ground cover
+dat_all %>% 
+  filter(!is.na(BareGroundCover)) %>% 
+  ggplot() +
+  geom_point(aes(Lon, Lat, col = Source), alpha = .5) 
