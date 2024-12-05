6b0c78ba587d1a0cab72ed90aec3abd987a7048f
Alice Stears <alice.e.stears@gmail.com>
2024-12-05 10:05:40 -0700

    updating ecoregion classification model

--- Analysis/VegComposition/ModelFitting/01_EcoregionClassification.R
+++ Analysis/VegComposition/ModelFitting/01_EcoregionClassification.R
@@ -4,258 +4,155 @@
 # 7/30/24
 #///////////////////
 
-
 # Load Packages -----------------------------------------------------------
 library(tidyverse)
 library(sf)
-library(ranger)
+library(nnet)
 
 # Load Data ---------------------------------------------------------------
 # data ready for modeling 
-modDat <- readRDS("./data/DataForModels_withEcoregion.rds")
+modDat <- readRDS("./Data_processed/CoverData/DataForModels_spatiallyAveraged_withSoils_noSf.rds")
 
 # Lump Ecoregions ---------------------------------------------------------
 # (eastern forest) put eastern temperate forests, tropical wet forests and northern forests together
 # (dry shrub and grass) put great plains, southern semiarid highlands, Mediterranean California and north american deserts together 
 # (western forest) put northwestern forested mountains, marine west coast forests together, and temperate sierras together
 
-# make a lookup table
-ecoReg_lu <- data.frame("NA_L1NAME" = unique(modDat$NA_L1NAME), "newRegion" = c("westForest", "dryShrubGrass", "westForest", "dryShrubGrass", "dryShrubGrass", "dryShrubGrass",
-                                                                             "westForest", NA, "eastForest", "eastForest", "eastForest", NA))
-# add to main data.frame 
-modDat_1 <- modDat %>% 
-  left_join(ecoReg_lu) %>% 
-  mutate(newRegion = as.factor(newRegion)) 
-
-## save for later
-saveRDS(modDat_1, file = "./data/DataForModels_withSubEcoreg.rds")
-
-modDat_fit <- modDat_x1  %>% 
-  select(newRegion, swe_meanAnnAvg_30yr , annWetDegDays_meanAnnAvg_30yr , 
+modDat_fit <- modDat  %>% 
+  mutate(newRegion = as.factor(newRegion)) %>% 
+  dplyr::select(newRegion, swe_meanAnnAvg_30yr , annWetDegDays_meanAnnAvg_30yr , 
             tmean_meanAnnAvg_30yr , isothermality_meanAnnAvg_30yr , 
-          prcp_meanAnnTotal_30yr , PrecipTempCorr_meanAnnAvg_30yr) %>% 
+          prcp_meanAnnTotal_30yr , PrecipTempCorr_meanAnnAvg_30yr, 
+         soilDepth                  , surfaceClay_perc          ,                
+         avgSandPerc_acrossDepth    ,  avgCoarsePerc_acrossDepth,                 
+         avgOrganicCarbonPerc_0_3cm ,  totalAvailableWaterHoldingCapacity, Long, Lat) %>% 
   drop_na() %>% 
   st_drop_geometry() %>% 
   slice_sample(n = 50000, replace = FALSE)
 
-modDat_test <-modDat %>% left_join(ecoReg_lu) %>% 
+modDat_test <- modDat %>% 
   mutate(newRegion = as.factor(newRegion)) %>% 
-  select(newRegion, swe_meanAnnAvg_30yr , annWetDegDays_meanAnnAvg_30yr , 
+  dplyr::select(newRegion, swe_meanAnnAvg_30yr , annWetDegDays_meanAnnAvg_30yr , 
          tmean_meanAnnAvg_30yr , isothermality_meanAnnAvg_30yr , 
-         prcp_meanAnnTotal_30yr , PrecipTempCorr_meanAnnAvg_30yr) %>% 
-  drop_na() %>% 
+         prcp_meanAnnTotal_30yr , PrecipTempCorr_meanAnnAvg_30yr, 
+         
+         soilDepth                  , surfaceClay_perc          ,                
+         avgSandPerc_acrossDepth    ,  avgCoarsePerc_acrossDepth,                 
+         avgOrganicCarbonPerc_0_3cm ,  totalAvailableWaterHoldingCapacity,
+         Long, Lat) %>% 
+  drop_na() %>%
   anti_join(modDat_fit)
-# # get ecoregion shapefiles 
-# ecoReg <- st_read("./data/Level1Ecoregions/", layer = "NA_CEC_Eco_Level1")
-# ecoReg_1 <- ecoReg %>% left_join(ecoReg_lu)
-# ggplot(ecoReg_1) + 
-#   geom_sf(aes(col = newRegion))
 
-
-# Fit Basic Random forest classification algorithm ------------------------
-set.seed(12011993)
-testMod <- ranger(newRegion ~  swe_meanAnnAvg_30yr + annWetDegDays_meanAnnAvg_30yr + 
-                    tmean_meanAnnAvg_30yr + isothermality_meanAnnAvg_30yr + prcp_meanAnnTotal_30yr + PrecipTempCorr_meanAnnAvg_30yr
-                    , data = modDat_fit, write.forest = TRUE, 
-                  importance = "permutation",
-                  #scale.permutation.importance = TRUE
+# Fit a simple regression (multinomial log-normal regression) ------------------------
+# following this example
+modDat_fit$newRegionFact <- relevel(modDat_fit$newRegion, ref = "dryShrubGrass")
+testMod <- nnet::multinom(newRegionFact ~  swe_meanAnnAvg_30yr + annWetDegDays_meanAnnAvg_30yr + 
+                    tmean_meanAnnAvg_30yr + isothermality_meanAnnAvg_30yr + prcp_meanAnnTotal_30yr + 
+                      PrecipTempCorr_meanAnnAvg_30yr +  soilDepth + surfaceClay_perc +             
+                    avgSandPerc_acrossDepth + avgCoarsePerc_acrossDepth +              
+                    avgOrganicCarbonPerc_0_3cm + totalAvailableWaterHoldingCapacity,
+                    data = modDat_fit
                   )
 
+summary(testMod)
 
-# test goodness of fit ----------------------------------------------------
-# get the model confusion matrix
-print(testMod$confusion.matrix)
-#                       predicted
-# true            dryShrubGrass eastForest westForest
-# dryShrubGrass         24912         84        672
-# eastForest               35      10786          0
-# westForest              941          1      12569
+head(pp <- fitted(testMod))
 
-# look at variable importance
-head(importance_pvalues(testMod, formula = newRegion ~  swe_meanAnnAvg_30yr + annWetDegDays_meanAnnAvg_30yr + 
-                          tmean_meanAnnAvg_30yr + isothermality_meanAnnAvg_30yr + prcp_meanAnnTotal_30yr + 
-                          PrecipTempCorr_meanAnnAvg_30yr, 
-                        data = modDat_1, method = "altmann"))
-# output: 
-#                                importance     pvalue
-# swe_meanAnnAvg_30yr             0.1651012 0.00990099
-# annWetDegDays_meanAnnAvg_30yr   0.2189938 0.00990099
-# tmean_meanAnnAvg_30yr           0.1736015 0.00990099
-# isothermality_meanAnnAvg_30yr   0.1384066 0.00990099
-# prcp_meanAnnTotal_30yr          0.3283871 0.00990099
-# PrecipTempCorr_meanAnnAvg_30yr  0.2304628 0.00990099
+# test the predictions of the model
+predictionDat <- modDat_test[,c("newRegion"                     , "swe_meanAnnAvg_30yr"           , "annWetDegDays_meanAnnAvg_30yr",     
+                                "tmean_meanAnnAvg_30yr"         , "isothermality_meanAnnAvg_30yr" , "prcp_meanAnnTotal_30yr"       ,     
+                                "PrecipTempCorr_meanAnnAvg_30yr", "soilDepth"                     , "surfaceClay_perc"             ,     
+                                 "avgSandPerc_acrossDepth"      ,  "avgCoarsePerc_acrossDepth"    ,  "avgOrganicCarbonPerc_0_3cm"  ,      
+                                 "totalAvailableWaterHoldingCapacity"
+)]
+predictionDatTemp <- cbind(predictionDat, predict(testMod, newdata = predictionDat, "probs"))
 
-# get predictions from the model
+# for each observation, determine which category has the highest predicted probability
+(temp <- predictionDatTemp %>% 
+  mutate(newRegion_Fact = as.factor(newRegion),
+    newRegion_predicted = pmap_dbl(.[c("dryShrubGrass", "eastForest", "westForest")], max), 
+         across(dryShrubGrass:westForest, ~.x == newRegion_predicted)) %>% 
+  mutate(dryShrubGrass = str_replace(dryShrubGrass, 'TRUE', 'dryShrubGrass'), 
+         eastForest = str_replace(eastForest, 'TRUE', "eastForest"), 
+         westForest = str_replace(westForest, 'TRUE', "westForest"),
+         dryShrubGrass = str_replace(dryShrubGrass, 'FALSE', ""), 
+         eastForest = str_replace(eastForest, 'FALSE', ""), 
+         westForest = str_replace(westForest, 'FALSE', "")) %>% 
+    mutate(, 
+           newRegion_group = as.factor(pmap_chr(.[c("dryShrubGrass", "eastForest", "westForest")], paste0))) %>% 
+    cbind(modDat_test %>% 
+            dplyr::select(Long, Lat)))
+# are these predictions the same as the actual classifications
+temp$goodPred <- temp$newRegion_Fact == temp$newRegion_group
+# ggplot(temp) + 
+#   geom_point(aes(Long, Lat, col = goodPred), alpha = .5) + 
+#   ggtitle("Locations of incorrect classifications")
+# ggplot(temp) + 
+#   geom_point(aes(Long, Lat, col = newRegionFact), alpha = .5) + 
+#   ggtitle("true ecoregion classification")
+ggplot(temp) + 
+  geom_point(aes(Long, Lat, col = newRegion_group), alpha = .5) + 
+  ggtitle("model-predicted ecoregion classification", 
+          subtitle = paste0("black dots indicate misclassification; ", round((100-sum(!temp$goodPred)/nrow(temp) * 100),1), "% classification accuracy" )) + 
+  geom_point(data = temp[temp$goodPred == FALSE,], aes(Long, Lat, col = goodPred), col = "black")
 
-  preds <- predict(testMod, data=st_drop_geometry(modDat_test))
-
-# look at the spatial distribution of the predictions 
-modPreds <- modDat_test 
-modPreds$newRegion_pred <- preds$predictions
-modPreds$BadPred<- ifelse(modPreds$newRegion == modPreds$newRegion_pred, NA, TRUE)
-
-# see where the forest is predicting incorrectly
-ggplot() + 
-  geom_sf(data = modPreds, aes(col = newRegion_pred), alpha = .3, pch = 20) + 
-  geom_sf(data = modPreds[modPreds$BadPred == TRUE,], aes(#col = newRegion_pred
-                                                          ), pch = 20, col = "black")
+## the predictions are ok, but not great 
 
 
-saveRDS(testMod, file = "./Analysis/VegComposition/ModelFitting/models/randForestClassification.rds")
 
-# Get Model  --------------------------------------------------------------
-treeMod <- treeInfo(testMod)
+# Classification Tree -----------------------------------------------------
+## first, try using rpart R package
+#https://www.datacamp.com/doc/r/cart
+#https://cran.r-project.org/web/packages/party/vignettes/party.pdf
+library(rpart)
+# fit a basic classification tree model
+treeMod <- rpart(newRegionFact ~  swe_meanAnnAvg_30yr + annWetDegDays_meanAnnAvg_30yr + 
+                   tmean_meanAnnAvg_30yr + isothermality_meanAnnAvg_30yr + prcp_meanAnnTotal_30yr + 
+                   PrecipTempCorr_meanAnnAvg_30yr +  soilDepth + surfaceClay_perc +             
+                   avgSandPerc_acrossDepth + avgCoarsePerc_acrossDepth +              
+                   avgOrganicCarbonPerc_0_3cm + totalAvailableWaterHoldingCapacity,
+                 data = modDat_fit, method = 'class')
+print(treeMod)
 
-# ## can I reconstruct the decision tree? 
-# for (i in 1:100000){#nrow(modDat_test)) {
-#   dat <- modDat_test[i,]
-#   # node 0
-#   if (dat$swe_meanAnnAvg_30yr <= 1.924646e+01) {
-#     # node 1
-#     if (dat$tmean_meanAnnAvg_30yr <= 6.712225e+00) {
-#       # node 3
-#       if (dat$tmean_meanAnnAvg_30yr <= 5.686742e+00 ) {
-#         # node 7
-#         if (dat$swe_meanAnnAvg_30yr <= 1.288767e-02) {
-#           # node 15
-#           if (dat$PrecipTempCorr_meanAnnAvg_30yr <= 1.384537e-01) {
-#             # node 31
-#           } else {
-#             # node 32
-#           }
-#         } else {
-#           # node 16
-#           if (dat$PrecipTempCorr_meanAnnAvg_30yr <=  -2.662799e-01) {
-#             # node 33
-#           } else {
-#             # node 34
-#           }
-#         }
-#       } else {
-#         # node 8
-#         if (dat$swe_meanAnnAvg_30yr <= 2.154517e+00) {
-#           # node 17
-#           if (dat$PrecipTempCorr_meanAnnAvg_30yr <= -3.362111e-01 ) {
-#             # node 35
-#           } else {
-#             # node 36
-#           }
-#         } else {
-#           # node 18
-#           if (dat$prcp_meanAnnTotal_30yr <= 6.616515e+02) {
-#             # node 37
-#           } else {
-#             # node 38
-#           }
-#         }
-#       }
-#     } else {
-#       # node 4
-#       if (dat$prcp_meanAnnTotal_30yr  <= 4.419938e+02 ) {
-#         # node 9
-#         if (dat$prcp_meanAnnTotal_30yr <= 3.616855e+02) {
-#           # node 19
-#           if (dat$PrecipTempCorr_meanAnnAvg_30yr <= 2.898600e-01) {
-#             # node 39
-#           } else {
-#             # node 40
-#           }
-#         } else {
-#           # node 20
-#           if (dat$PrecipTempCorr_meanAnnAvg_30yr <= 3.719435e-01) {
-#             # node 41
-#           } else {
-#             # node 42
-#           }
-#         }
-#       } else {
-#         # node 10
-#         if (dat$prcp_meanAnnTotal_30yr <= 1.128762e+03 ) {
-#           # node 21
-#           if (dat$PrecipTempCorr_meanAnnAvg_30yr <= 7.278897e-02) {
-#             # node 43
-#           } else {
-#             # node 44
-#           }
-#         } else {
-#           # node 22
-#           if (dat$PrecipTempCorr_meanAnnAvg_30yr <= -4.500292e-01){
-#             # node 45
-#           } else {
-#             # node 46
-#           }
-#         }
-#       }
-#     }
-#   } else {
-#     # node 2
-#     if (dat$isothermality_meanAnnAvg_30yr <= -2.949601e+01 ) {
-#       # node 5
-#       if (dat$swe_meanAnnAvg_30yr <=  4.628927e+01) {
-#         # node 11
-#         if (dat$isothermality_meanAnnAvg_30yr <=  -3.173486e+01) {
-#           # node 23
-#           if (dat$prcp_meanAnnTotal_30yr <= 5.322318e+02) {
-#             # node 47
-#           } else {
-#             # node 48
-#           }
-#         } else {
-#           # node 24
-#           if (dat$PrecipTempCorr_meanAnnAvg_30yr <= -2.553431e-02){
-#             # node 49
-#           } else {
-#             # node 50
-#           }
-#         }
-#       } else {
-#         # node 12
-#         if (dat$PrecipTempCorr_meanAnnAvg_30yr <= 7.727684e-02 ) {
-#           # node 25
-#           if (dat$swe_meanAnnAvg_30yr <=  7.067461e+01) {
-#             # node 51
-#           } else {
-#             # node 52
-#           }
-#         } else {
-#           # node 26
-#           if (dat$isothermality_meanAnnAvg_30yr <= -3.178565e+01) {
-#             # node 53
-#           } else {
-#             # node 54
-#           }
-#         }
-#       }
-#     } else {
-#       #  node 6
-#       if (dat$PrecipTempCorr_meanAnnAvg_30yr <= -1.712363e-01) {
-#         # node 13
-#         if (dat$prcp_meanAnnTotal_30yr <= 7.168480e+02) {
-#           # node 27
-#           if (dat$PrecipTempCorr_meanAnnAvg_30yr <= -2.827414e-01) {
-#             # node 55
-#           } else {
-#             # node 56
-#           }
-#         } else {
-#           # node 28
-#           if (dat$tmean_meanAnnAvg_30yr <= 5.162721e+00) {
-#             # node 57
-#           } else {
-#             # node 58
-#           }
-#         }
-#       } else {
-#         # node 14
-#         if (dat$tmean_meanAnnAvg_30yr <= 4.754435e+00) {
-#           # node 29
-#           print(c(as.character(dat$newRegion), "westForest"))
-#         } 
-#         else {
-#           # node 30
-#         }
-#       }
-#     }
-#   }
-# }
+# prune the tree
+# first, find the cross-validation error
+printcp(treeMod)
+treeMod_p <- prune(treeMod, cp = .01)
+print(treeMod_p)
+summary(treeMod_p)
+par(xpd = TRUE)
+plot(treeMod_p, compress = TRUE)
+text(treeMod_p, use.n = TRUE)
+
+## now, try using party r package (conditional inference, nonparametric regression tree)
+library(party)
+partyTree <- ctree(newRegionFact ~  swe_meanAnnAvg_30yr + annWetDegDays_meanAnnAvg_30yr + 
+                     tmean_meanAnnAvg_30yr + isothermality_meanAnnAvg_30yr + prcp_meanAnnTotal_30yr + 
+                     PrecipTempCorr_meanAnnAvg_30yr +  soilDepth + surfaceClay_perc +             
+                     avgSandPerc_acrossDepth + avgCoarsePerc_acrossDepth +              
+                     avgOrganicCarbonPerc_0_3cm + totalAvailableWaterHoldingCapacity,
+                   data = modDat_fit)
+# try restricting the tree depth (too long right now)
+partyTree <- ctree(newRegionFact ~  swe_meanAnnAvg_30yr + annWetDegDays_meanAnnAvg_30yr + 
+                     tmean_meanAnnAvg_30yr + isothermality_meanAnnAvg_30yr + prcp_meanAnnTotal_30yr + 
+                     PrecipTempCorr_meanAnnAvg_30yr +  soilDepth + surfaceClay_perc +             
+                     avgSandPerc_acrossDepth + avgCoarsePerc_acrossDepth +              
+                     avgOrganicCarbonPerc_0_3cm + totalAvailableWaterHoldingCapacity,
+                   data = modDat_fit, controls = ctree_control(maxdepth = 7))
+partyTree
+plot(partyTree, main="Conditional Inference Tree for ecoregions")
+# predict categories based on this simple model
+partyTreePreds <- cbind(predictionDat, "partyPrediction" = Predict(partyTree, newdata = predictionDat)) %>% 
+  cbind(modDat_test %>% 
+          dplyr::select(Long, Lat))
+partyTreePreds$goodPred <- partyTreePreds$newRegion == partyTreePreds$partyPrediction
+# percentage of misclassifications
+sum(!partyTreePreds$goodPred)/nrow(partyTreePreds) * 100
+
+ggplot(partyTreePreds) + 
+  geom_point(aes(Long, Lat, col = partyPrediction), alpha = .5) + 
+  ggtitle("model-predicted ecoregion classification", 
+          subtitle = paste0("black dots indicate misclassification; ", round((100-sum(!partyTreePreds$goodPred)/nrow(partyTreePreds) * 100),1), "% classification accuracy" )) + 
+  geom_point(data = partyTreePreds[partyTreePreds$goodPred == FALSE,], aes(Long, Lat, col = goodPred), col = "black")
+ 
 
