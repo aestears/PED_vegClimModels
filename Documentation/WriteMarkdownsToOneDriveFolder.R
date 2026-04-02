4874c8a22233a2156c847565424a8e8df10f14b0
aestears <alice.e.stears@gmail.com>
2026-04-02 15:05:09 -0600

    updating methods overview document to include visualizations of LCMAP exclusion as well as general tidying of repo

---
+++ Documentation/WriteMarkdownsToOneDriveFolder.R
@@ -0,0 +1,11 @@
+# This script runs R Markdown documentation docs. and saves them in the shared oneDrive folder /VegClim_modelOutputs/Documentation/
+
+
+# Methods overview document -----------------------------------------------
+rmarkdown::render(input = "./Documentation/CoverMethodsOverview.Rmd", 
+                  output_format = "html_document", 
+                  output_dir = "/Users/astears/Library/CloudStorage/OneDrive-DOI/VegClim_modelOutputs/Documentation/",
+                  output_file = paste0("./CoverMethodsOverview_",Sys.Date(),".html"))
+# save in additional local documentation folder
+file.copy(from = "/Users/astears/Library/CloudStorage/OneDrive-DOI/VegClim_modelOutputs/Documentation/CoverMethodsOverview_2026-04-02.html", 
+              to = paste0("./Documentation/CoverMethodsOverview_",Sys.Date(),".html"))
